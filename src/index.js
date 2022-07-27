e93574309dd9e23a458425b9fc3dbd2763ae6f97
Letam Bossman Barinua <letambarinua@gmail.com>
2022-07-27 20:37:52 +0100

    Add class to edit button

--- src/index.js
+++ src/index.js
@@ -225,6 +225,7 @@ function listenForNewProjectEvents() {
         const deleteOrEditDiv = document.createElement("div");
         deleteOrEditDiv.classList.add("delete-edit-div");
         const editProject = document.createElement("p");
+        editProject.classList.add('edit');
         editProject.textContent = "Edit";
         const deleteProject = document.createElement("p");
         deleteProject.textContent = "Delete";
