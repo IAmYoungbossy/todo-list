119b4d080f7efeaf8cd76644d7b02f5a9801e709
Letam Bossman Barinua <letambarinua@gmail.com>
2022-07-29 18:00:45 +0100

    Add tasks section and addtask button

--- src/index.js
+++ src/index.js
@@ -299,3 +299,17 @@ function editCurentProject() {
 }
 window.addEventListener("mouseup", editCurentProject);
 document.addEventListener("mousedown", removeDeleteAndEditPopUp);
+
+const mainSection = document.querySelector('.mainSection');
+const addTaskSection = document.createElement('div');
+const taskList = document.createElement('ul');
+const addTaskButton = document.createElement('li');
+const myAddTask = new Image;
+myAddTask.src = AddTask;
+addTaskButton.append(myAddTask, "Add Task");
+taskList.appendChild(addTaskButton);
+addTaskSection.appendChild(taskList);
+mainSection.appendChild(addTaskSection);
+addTaskSection.classList.add('add-task-section');
+taskList.classList.add('task-list');
+addTaskButton.classList.add('add-task-button');
