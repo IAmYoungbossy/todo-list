8c22bf4e38c79f038ee4819428ae68c2bc98b2b4
Letam Bossman Barinua <letambarinua@gmail.com>
2022-07-31 16:16:45 +0100

    Add the correct path to date element in toggles state module

--- src/toggle-states.js
+++ src/toggle-states.js
@@ -1,18 +1,17 @@
 function toggleReadOrImportance(e) {
-  if (e.target.className == "unchecked-star") {
-    console.log(e.target.parentNode.parentNode.childNodes[2].childNodes[1]);
+  if (e.target.classList[0] == "unchecked-star") {
     e.target.parentNode.parentNode.childNodes[2].childNodes[1].classList.toggle(
       "check"
     );
   }
   if (e.target.className == "undone-task") {
     e.target.parentNode.parentNode.childNodes[0].childNodes[1].classList.toggle(
-      "check"
+      "done"
     );
     e.target.parentNode.parentNode.childNodes[1].childNodes[1].classList.toggle(
       "line-through"
     );
-    e.target.parentNode.parentNode.childNodes[2].childNodes[0].classList.toggle(
+    e.target.parentNode.parentNode.parentNode.childNodes[0].childNodes[3].childNodes[0].childNodes[0].classList.toggle(
       "line-through"
     );
     e.target.parentNode.parentNode.childNodes[1].childNodes[0].classList.toggle(
