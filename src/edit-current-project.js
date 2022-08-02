0837e5e0d8c0bdc3faf1134b074ac74d07882ee3
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-02 14:13:56 +0100

    Add function enableEditing in edit-current-project.js

--- src/edit-current-project.js
+++ src/edit-current-project.js
@@ -1,60 +1,65 @@
 import { getProjectInput } from "./project-input";
 
-function editCurentProject() {
-  const editButton = document.querySelector(".edit");
+function enableEditing() {
+  const project = document.querySelector(".edit").parentNode.parentNode;
+  const projectList = document.querySelector(".project-list");
 
-  if (!!editButton) {
-    const project = editButton.parentNode.parentNode;
-    const projectList = document.querySelector(".project-list");
-    for (let i = 0; i < projectList.childNodes.length; i++) {
-      if (projectList.childNodes[i].classList[0] == "input-Li") return;
+  for (let i = 0; i < projectList.childNodes.length; i++)
+    if (projectList.childNodes[i].classList[0] == "input-Li") return;
+
+  const getName = (function () {
+    project.classList.add("hidden");
+    const children = project.childNodes;
+    const childrenArray = Array.from(children);
+    let previousName = childrenArray[1].data;
+    if (previousName === undefined) {
+      const childArray = Array.from(childrenArray[1].childNodes);
+      previousName = childArray[0].data;
     }
+    return { previousName };
+  })();
 
-    const getName = (function () {
-      project.classList.add("hidden");
-      const children = project.childNodes;
-      const childrenArray = Array.from(children);
-      let previousName = childrenArray[1].data;
-      if (previousName === undefined) {
-        const childArray = Array.from(childrenArray[1].childNodes);
-        previousName = childArray[0].data;
-      }
-      return { previousName };
-    })();
+  getProjectInput();
 
-    getProjectInput();
+  const insertGetProjectInput = (function () {
+    const previousProject =
+      document.querySelector(".hidden").nextElementSibling;
+    const nameInput = document.querySelector(".name-input");
+    const inputForm = document.querySelector(".input-Li");
+    nameInput.value = getName.previousName;
+    projectList.insertBefore(inputForm, previousProject);
+    return {
+      inputForm,
+      nameInput,
+    };
+  })();
 
-    const inserInputForm = (function () {
-      const previousProject =
-        document.querySelector(".hidden").nextElementSibling;
-      const nameInput = document.querySelector(".name-input");
-      const inputForm = document.querySelector(".input-Li");
-      nameInput.value = getName.previousName;
-      projectList.insertBefore(inputForm, previousProject);
-      return {
-        inputForm,
-        nameInput,
-      };
-    })();
+  function updateNewProjectName() {
+    const newProjectName = document.createElement("p");
+    newProjectName.textContent = insertGetProjectInput.nameInput.value;
+    project.replaceChild(newProjectName, project.childNodes[1]);
+    removeInputForm();
+    project.classList.remove("hidden");
+  }
 
-    function updateNewProjectName() {
-      const newProjectName = document.createElement("p");
-      newProjectName.textContent = inserInputForm.nameInput.value;
-      project.replaceChild(newProjectName, project.childNodes[1]);
-      removeInputForm();
-      project.classList.remove("hidden");
-    }
+  function removeInputForm() {
+    insertGetProjectInput.inputForm.parentNode.removeChild(
+      insertGetProjectInput.inputForm
+    );
+    project.classList.remove("hidden");
+  }
 
-    function removeInputForm() {
-      inserInputForm.inputForm.parentNode.removeChild(inserInputForm.inputForm);
-      project.classList.remove("hidden");
-    }
-
+  (() => {
     const add = document.querySelector(".add");
     const cancel = document.querySelector(".cancel");
     cancel.addEventListener("mousedown", removeInputForm);
     add.addEventListener("mousedown", updateNewProjectName);
-  }
+  })();
 }
 
+function editCurentProject() {
+  if (!!document.querySelector(".edit")) {
+    enableEditing();
+  }
+}
 export { editCurentProject };
