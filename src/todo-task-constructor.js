676953e5a51f820a955536819119b7734f9271d4
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-03 12:06:41 +0100

    Create a TodoTask constructor function in todo-task-constructor.js

---
+++ src/todo-task-constructor.js
@@ -0,0 +1,7 @@
+function TodoTask (title, desc, date, completed, important) {
+  this.title = title;
+  this.desc = desc;
+  this.date = date;
+  this.completed = completed;
+  this.important = important;
+}
