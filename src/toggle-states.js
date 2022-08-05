6381e47de8123ad11eaace85a854553dbcea9441
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-05 17:49:35 +0100

    Edit the code in toggleReadOrImportance() in toggle-state.js
    The path to date was incorrect which was corrected with this
    update.

--- src/toggle-states.js
+++ src/toggle-states.js
@@ -1,9 +1,8 @@
 function toggleReadOrImportance(e) {
-  if (e.target.classList[0] == "unchecked-star") {
+  if (e.target.classList[0] == "unchecked-star")
     e.target.parentNode.parentNode.childNodes[2].childNodes[1].classList.toggle(
       "check"
     );
-  }
   if (e.target.className == "undone-task") {
     e.target.parentNode.parentNode.childNodes[0].childNodes[1].classList.toggle(
       "done"
@@ -11,7 +10,7 @@ function toggleReadOrImportance(e) {
     e.target.parentNode.parentNode.childNodes[1].childNodes[1].classList.toggle(
       "line-through"
     );
-    e.target.parentNode.parentNode.parentNode.childNodes[0].childNodes[3].childNodes[0].childNodes[0].classList.toggle(
+    e.target.parentNode.parentNode.childNodes[3].childNodes[0].childNodes[0].classList.toggle(
       "line-through"
     );
     e.target.parentNode.parentNode.childNodes[1].childNodes[0].classList.toggle(
