2807955edeb80656593b0a206f2c9ec349051869
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-05 13:04:48 +0100

    Add removeTaskFromList function in add-or-cancel-task-event.js
    This clears up the list the populate the screen with updated list
    from array.

--- src/add-or-cancel-task-event.js
+++ src/add-or-cancel-task-event.js
@@ -6,38 +6,50 @@ import { TodoTask } from "./project-constructor";
 function addAndCancelTask(e) {
   const taskList = document.querySelector(".task-list");
   const inputTaskForm = document.querySelector(".task-input-form");
+
   if (e.target.className == "add-task-button") {
     for (let i = 0; i < taskList.childNodes.length; i++) {
       if (taskList.childNodes[i].classList[0] == "task-input-form") return;
     }
     newTaskInput();
   }
+
   if (e.target.className == "cancel-task") {
     inputTaskForm.parentNode.removeChild(inputTaskForm);
   }
+
   if (e.target.className == "add-task") {
     if (document.querySelector(".title-input-form").value.trim() === "") return;
     if (document.querySelector(".date-input-form").value.trim() === "") return;
+
     pushToTaskArray();
+    removeTaskFromList();
     displayTasks();
-    inputTaskForm.parentNode.removeChild(inputTaskForm);
   }
 }
 
 function displayTasks() {
-  currentProjectArray.forEach((task) => {
-    displayAddedTasks(task.title, task.desc, task.date);
-  });
+  currentProjectArray.forEach((task) =>
+    displayAddedTasks(task.title, task.desc, task.date)
+  );
 }
 
-function pushToTaskArray(){
+function pushToTaskArray() {
   currentProjectArray.push(
     new TodoTask(
-      document.querySelector(".date-input-form").value,
       document.querySelector(".title-input-form").value,
-      document.querySelector(".text-area-form").value
+      document.querySelector(".text-area-form").value,
+      document.querySelector(".date-input-form").value
     )
   );
+}
+
+function removeTaskFromList() {
+  const taskList = document.querySelector(".task-list");
+  if (!!taskList) {
+    while (taskList.childNodes.length > 1)
+      taskList.removeChild(taskList.firstChild);
+  }
 }
 
 export { addAndCancelTask };
