4289191f787611c4a0fa4105648ff4d2177e0e8a
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-06 15:10:24 +0100

    Convert function constructors to class

--- src/project-constructor.js
+++ src/project-constructor.js
@@ -1,34 +1,35 @@
 let projectArray = [];
 
-function Project(projectName) {
-  this.name = projectName;
-  this.taskArray = [];
+class Project {
+  constructor(projectName) {
+    this.name = projectName;
+    this.taskArray = [];
+  }
+  setName(newName) {
+    this.name = newName;
+  }
 }
 
-Project.prototype.setName = function (newName) {
-  this.name = newName;
-};
 
-function TodoTask(title, desc, date) {
-  this.title = title;
-  this.desc = desc;
-  this.date = date;
-  this.completed;
-  this.important;
+class TodoTask {
+  constructor(title, desc, date) {
+    this.title = title;
+    this.desc = desc;
+    this.date = date;
+    this.completed;
+    this.important;
+  }
+  setDetails(newTitle, newDesc, newDate) {
+    this.title = newTitle;
+    this.desc = newDesc;
+    this.date = newDate;
+  }
+  toggleComplete() {
+    this.completed ? (this.completed = false) : (this.completed = true);
+  }
+  toggleImportance() {
+    this.important ? (this.important = false) : (this.important = true);
+  }
 }
-
-TodoTask.prototype.setDetails = function (newTitle, newDesc, newDate) {
-  this.title = newTitle;
-  this.desc = newDesc;
-  this.date = newDate;
-};
-
-TodoTask.prototype.toggleComplete = function () {
-  this.completed ? (this.completed = false) : (this.completed = true);
-};
-
-TodoTask.prototype.toggleImportance = function () {
-  this.important ? (this.important = false) : (this.important = true);
-};
 
 export { projectArray, Project, TodoTask };
