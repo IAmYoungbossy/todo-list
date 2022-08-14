17e8bc4f5a0cc39e6eb56b8e68c1206375dd2a6d
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-14 22:39:32 +0100

    Add Icon credit to footer

--- src/page-footer.js
+++ src/page-footer.js
@@ -3,18 +3,28 @@ function createfooter() {
   const footer = document.createElement("footer");
   const anchorLink = document.createElement("a");
   const gitHubIcon = document.createElement("i");
+  const iconsCredit = document.createElement("p");
+  const flatIconAnchor = document.createElement("a");
+  const icons8Anchor = document.createElement("a");
 
+  flatIconAnchor.textContent = " Flaticon ";
+  icons8Anchor.textContent = " Icons8 ";
+  iconsCredit.append("Icons by", flatIconAnchor, " & ", icons8Anchor);
   anchorLink.appendChild(gitHubIcon);
-  footer.append("Copyright \u00A9 2022 IAmYoungbossy");
-  footer.appendChild(anchorLink);
-  body.appendChild(footer);
 
-  anchorLink.setAttribute(
-    "href",
-    "https:////github.com/IAmYoungbossy/calculator"
-  );
-
+  flatIconAnchor.setAttribute("target", "_blank");
+  icons8Anchor.setAttribute("target", "_blank");
+  flatIconAnchor.setAttribute("href", "https://www.flaticon.com/");
+  icons8Anchor.setAttribute("href", "https://icons8.com/");
+  anchorLink.setAttribute("href", "https://github.com/IAmYoungbossy/todo-list");
   gitHubIcon.classList.add("fa", "fa-github");
+  footer.append(
+    "Copyright \u00A9 2022 IAmYoungbossy",
+    "  ",
+    anchorLink,
+    iconsCredit
+  );
+  body.appendChild(footer);
 }
 
 export { createfooter };
