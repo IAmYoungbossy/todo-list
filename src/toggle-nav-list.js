9fa90a66cbc65e3abc20aeccdcc730b96ac9373f
Letam Bossman Barinua <letambarinua@gmail.com>
2022-08-14 09:27:34 +0100

    Add position absolute on home and project classes just as in nav
    When the nav menu is toggled to hidden because the content of the
    nav bar is still relative it takes up space on the page so to
    solve that I set them to absolute.

--- src/toggle-nav-list.js
+++ src/toggle-nav-list.js
@@ -2,6 +2,8 @@ const toggleNavList = (function () {
   document.addEventListener("mouseup", (e) => {
     if (e.target.parentNode.className === "menuIcon") {
       document.querySelector(".nav").classList.toggle("nav-hidden");
+      document.querySelector(".home").classList.toggle("nav-hidden");
+      document.querySelector(".project").classList.toggle("nav-hidden");
     }
   });
 })();
